a06087727f71ee4d8c9f781e0dc09aa9c24ffbc0
Anish <anishkakaiya@outlook.com>
2022-08-26 12:21:04 +0100

    add test

--- hello_test.go
+++ hello_test.go
@@ -6,17 +6,21 @@ func TestHello(t *testing.T) {
 	t.Run("Saying hello to people", func(t *testing.T) {
 		got := Hello("Anish")
 		want := "Hello, Anish!"
-	
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+
+		assertCorrectMessage(t, got, want)
 	})
 	t.Run("Say 'Hello, World!' when an empty string is inputted", func(t *testing.T) {
-		got := Hello()
+		got := Hello("")
 		want := "Hello, World!"
 
-		if got!= want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
