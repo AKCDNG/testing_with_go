0e93bc6efaecf35813945c7e2ba4db7aa870a7df
Anish <anishkakaiya@outlook.com>
2022-08-27 09:27:48 +0100

    refactor test and code

--- integers/adder.go
+++ integers/adder.go
@@ -1,9 +1,5 @@
 package integers
 
 func Add(x, y int) int {
-	if x == 2 {
-		return 4
-	} else {
-		return 10
-	}
+	return x + y
 }
--- integers/adder_test.go
+++ integers/adder_test.go
@@ -6,17 +6,21 @@ func TestAdder(t *testing.T) {
 	t.Run("Can return a value", func(t *testing.T) {
 		sum := Add(2, 2)
 		expected := 4
-		if sum != expected {
-			t.Errorf("expected '%d' but got '%d'", expected, sum)
-		}
+
+		assertCorrectValue(t, expected, sum)
 	})
 
 	t.Run("Can return 10 when we do 5 + 5", func(t *testing.T) {
 		sum := Add(5, 5)
 		expected := 10
 
-		if sum != expected {
-			t.Errorf("expected '%d' but got '%d'", expected, sum)
-		}
+		assertCorrectValue(t, expected, sum)
 	})
+}
+
+func assertCorrectValue(t testing.TB, expected, sum int) {
+	t.Helper()
+	if sum != expected {
+		t.Errorf("expected '%d' but got '%d'", expected, sum)
+	}
 }
