3fda30e4cac2b74741da2d345fc1c6d655ed9af8
Anish <anishkakaiya@outlook.com>
2022-08-27 09:20:34 +0100

    refactor test

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -3,10 +3,12 @@ package integers
 import "testing"
 
 func TestAdder(t *testing.T) {
-	sum := Add(2, 2)
-	expected := 4
+	t.Run("Can return a value", func(t *testing.T) {
+		sum := Add(2, 2)
+		expected := 4
+		if sum != expected {
+			t.Errorf("expected '%d' but got '%d'", expected, sum)
+		}
+	})
 
-	if sum != expected {
-		t.Errorf("expected '%d' but got '%d'", expected, sum)
-	}
 }
