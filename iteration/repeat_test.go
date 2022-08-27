bde68f8815723df38ca206c7c562ea10d69ed870
Anish <anishkakaiya@outlook.com>
2022-08-27 10:58:59 +0100

    refactor test

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -10,19 +10,22 @@ func TestRepeat(t *testing.T) {
 		repeated := Repeat("a", 5)
 		expected := "aaaaa"
 
-		if repeated != expected {
-			t.Errorf("expected %q but got %q", expected, repeated)
-		}
+		assertCorrectValue(t, expected, repeated)
 	})
 
 	t.Run("Can repeat a character a certain amount of times", func(t *testing.T) {
 		repeated := Repeat("a", 10)
 		expected := "aaaaaaaaaa"
 
-		if repeated != expected {
-			t.Errorf("expected %q but got %q", expected, repeated)
-		}
+		assertCorrectValue(t, expected, repeated)
 	})
+}
+
+func assertCorrectValue(t testing.TB, expected, repeated string) {
+	t.Helper()
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
 }
 
 func BenchmarkRepeat(b *testing.B) {
