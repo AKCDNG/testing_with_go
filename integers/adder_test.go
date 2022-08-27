4b3f9491ed90e85604a41c4a9f9d7a7ad1095fb1
Anish <anishkakaiya@outlook.com>
2022-08-27 09:23:39 +0100

    add test for other digits

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -11,4 +11,12 @@ func TestAdder(t *testing.T) {
 		}
 	})
 
+	t.Run("Can return 10 when we do 5 + 5", func(t *testing.T) {
+		sum := Add(5, 5)
+		expected := 10
+
+		if sum != expected {
+			t.Errorf("expected '%d' but got '%d'", expected, sum)
+		}
+	})
 }
