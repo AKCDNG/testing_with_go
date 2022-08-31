77dead2ab77eee05f4668dafc7ae5e7d73b04ad7
Anish <anishkakaiya@outlook.com>
2022-08-31 12:11:09 +0100

    add test case for area

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -10,3 +10,12 @@ func TestPerimeter(t *testing.T) {
 		t.Errorf("got %.2f want %.2f", got, want)
 	}
 }
+
+func TestArea(t *testing.T) {
+	got := Area(12.0, 12.0)
+	want := 144.00
+
+	if got!= want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
